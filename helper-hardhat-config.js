dcfed72163c80ba72dd95994bcf17d4f54713dfc
PutlaTheophila <putlatheophila123@gmail.com>
2025-05-29 21:49:02 +0530

    added dot env for hiding s_id of chainlink

--- helper-hardhat-config.js
+++ helper-hardhat-config.js
@@ -1,3 +1,5 @@
+require('dotenv').config();
+
 const {ethers} = require("hardhat");
 
 const networkConfig = {
@@ -5,7 +7,7 @@ const networkConfig = {
       name: "sepolia",
       VRFConsumerBaseV2Plusaddress: "0x9DdfaCa8183c41ad55329BdeeD9F6A8d53168B1B",
       keyHash: "0x787d74caea10b2b357790d5b5247c2f63d1d91572a9846f780606e4d953677ae",
-      subscriptionId : "43852619907888437924998710464537703569832687462981514791370309263181931267537",
+      subscriptionId :process.env.SEPOLIA_SUBSCRIPTION_ID,
       requestConfirmations : 3,
       numWords : 1,
       callbackGasLimit : 500000,
